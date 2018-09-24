bbd78d0d91e815b6b14e808cb6a0509d050b9049
Linda Xia <lindax@andrew.cmu.edu>
2018-09-24 14:18:43 -0400

    Finished lab 2

--- main.js
+++ main.js
@@ -1,7 +1,27 @@
 function addNewList() {
     alert('hello world!');
+    var list = document.getElementById("grocery-list");
+    var itemInput = document.getElementById("new-list-item");
+    var newItem = document.createElement("li");
+    newItem.appendChild(document.createTextNode(itemInput.value));
+    list.appendChild(newItem);
 }
 
 function addListItem() {
     console.log('hello world');
+}
+
+function deleteListItem(item) { 
+    // remove li element (item) from ol element (item.parentNode) 
+    item.parentNode.removeChild(item);
+}
+
+function completeListItem(item) { 
+    
+    if (item.checked) { 
+        item.parentNode.style.textDecoration = "line-through"; 
+    }
+    else { 
+        item.parentNode.style.textDecoration = "none";
+    }
 }
